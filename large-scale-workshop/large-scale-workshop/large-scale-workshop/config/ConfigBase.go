46eb8b7683f1179b5a83fb96e93f5edbcb6dd031
ananalem <74459272+ananalem@users.noreply.github.com>
2024-05-30 19:50:43 +0300

    Delete large-scale-workshop/large-scale-workshop/large-scale-workshop directory

--- large-scale-workshop/large-scale-workshop/large-scale-workshop/config/ConfigBase.go
+++
@@ -1,4 +0,0 @@
-package config
-type ConfigBase struct {
-	Type string `yaml:"type"` // <-- The struct tag: "type" key in YAML loads into Type field
-}
--- large-scale-workshop/large-scale-workshop/large-scale-workshop/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/sibaazab/large-scale-workshop.git
-
-go 1.22.2
-
-require gopkg.in/yaml.v2 v2.4.0
